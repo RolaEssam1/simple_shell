2553084df9a07b650a39066ab2b7bf055df2e569
rola <rolaessam03@gmail.com>
2023-09-20 14:33:42 -0700

    a

--- _atoi.c
+++ _atoi.c
@@ -1,40 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * factorial - calculates the factorial of a non-negative integer.
- * @n: The integer for which the factorial is to be calculated.
+ * factorial - returns the factorial of a number
+ * @n: number to return the factorial from
  *
- * Return: The factorial of n.
+ * Return: factorial of n
  */
-unsigned long long factorial(int n)
+int factorial(int n)
 {
 	if (n < 0)
-	{
-	/* Factorial is not defined for negative numbers. */
-	return (0);
-	}
-	else if (n == 0 || n == 1)
-	{
-	/* The factorial of 0 and 1 is 1.*/
-	return (1);
-	}
-	else
-	{
-	/* Recursive calculation for positive integers.*/
+		return (-1);
+	if (n == 0)
+		return (1);
 	return (n * factorial(n - 1));
-	}
-}
-
-int main(void)
-{
-	int n;
-
-	printf("Enter a non-negative integer: ");
-	scanf("%d", &n);
-
-	unsigned long long result = factorial(n);
-
-	printf("Factorial of %d is %llu\n", n, result);
-
-	return (0);
 }
