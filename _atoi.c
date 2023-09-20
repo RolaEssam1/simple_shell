d3f98fffed3164de49cf0f31ff4dc9413b38ab10
rola <rolaessam03@gmail.com>
2023-09-20 13:50:57 -0700

    a

--- _atoi.c
+++ _atoi.c
@@ -1,56 +1,67 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-/**
- * is_vowel - checks if a character is a vowel
- * @c: the character to check
- * Return: 1 if it's a vowel, 0 otherwise
- */
-int is_vowel(char c)
-{
-    char vowels[] = "AEIOUaeiou";
-    return strchr(vowels, c) != NULL;
+// Define a structure for a node in a linked list
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+// Function to insert a new node at the end of the linked list
+void insert(Node **head, int data) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    Node *temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
 }
 
-/**
- * reverse_string - reverses a given string
- * @str: the string to reverse
- */
-void reverse_string(char *str)
-{
-    int len = strlen(str);
-    for (int i = 0, j = len - 1; i < j; i++, j--) {
-        char temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
+// Function to display the linked list
+void display(Node *head) {
+    Node *current = head;
+
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
     }
+
+    printf("NULL\n");
 }
 
-int main()
-{
-    char input[100];
-    
-    printf("Enter a string: ");
-    fgets(input, sizeof(input), stdin);
-    
-    // Remove the trailing newline character
-    if (input[strlen(input) - 1] == '\n') {
-        input[strlen(input) - 1] = '\0';
-    }
+int main() {
+    Node *head = NULL;
 
-    printf("You entered: %s\n", input);
-    
-    printf("Reversed string: ");
-    reverse_string(input);
-    printf("%s\n", input);
+    // Insert nodes into the linked list
+    insert(&head, 10);
+    insert(&head, 20);
+    insert(&head, 30);
 
-    printf("Vowels in the string: ");
-    for (int i = 0; i < strlen(input); i++) {
-        if (is_vowel(input[i])) {
-            printf("%c ", input[i]);
-        }
+    // Display the linked list
+    printf("Linked List: ");
+    display(head);
+
+    // Free the dynamically allocated memory for the linked list nodes
+    Node *temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
     }
-    printf("\n");
 
     return 0;
 }
